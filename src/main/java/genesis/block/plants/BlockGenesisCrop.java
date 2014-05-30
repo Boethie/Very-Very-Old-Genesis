aab88cf2fca2e616158c4cb215374f4d73de0a83
websitenmaker12 <eidechse12@hotmail.com>
2014-05-30 21:09:37 +0200

    Removed error; Renamed Araucarioxylon Wood

--- src/main/java/genesis/block/plants/BlockGenesisCrop.java
+++ src/main/java/genesis/block/plants/BlockGenesisCrop.java
@@ -1,23 +1,18 @@
 package genesis.block.plants;
 
-import java.util.Random;
+import genesis.common.Genesis;
 
-import sun.rmi.log.ReliableLog.LogFile;
+import java.util.Random;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockCrops;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.World;
-
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
-
-import genesis.common.Genesis;
 
 public class BlockGenesisCrop extends BlockCrops {
 
